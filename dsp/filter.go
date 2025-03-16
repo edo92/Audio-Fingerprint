2b8d8bad2cc28d0f0825ac01bd3e48635ba73a04
edo92 <edoter92@gmail.com>
2025-03-16 09:13:03 -0700

    setup dsp - FIR low‐pass filter

---
+++ dsp/filter.go
@@ -0,0 +1,29 @@
+package dsp
+
+import (
+	"math"
+)
+
+func GenerateLowPassKernel(cutoffFreq float64, sampleRate int, numTaps int) []float64 {
+	kernel := make([]float64, numTaps)
+	fc := cutoffFreq / float64(sampleRate)
+	m := float64(numTaps - 1)
+	for i := 0; i < numTaps; i++ {
+		n := float64(i)
+		if n == m/2 {
+			kernel[i] = 2 * fc
+		} else {
+			kernel[i] = math.Sin(2*math.Pi*fc*(n-m/2)) / (math.Pi * (n - m/2))
+		}
+		kernel[i] *= 0.54 - 0.46*math.Cos(2*math.Pi*float64(i)/m)
+	}
+
+	sum := 0.0
+	for i := 0; i < numTaps; i++ {
+		sum += kernel[i]
+	}
+	for i := 0; i < numTaps; i++ {
+		kernel[i] /= sum
+	}
+	return kernel
+}
