3a02fe990fce70e0322912942868d4449f954480
edo92 <edoter92@gmail.com>
2025-03-16 09:07:32 -0700

    read wav file

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	audio "fingerprint/wav"
 	"fmt"
+	"log"
 )
 
 func main() {
-	fmt.Println("Main")
+	audioFile := "audio.wav"
+
+	samples, _, err := audio.ReadWavFile(audioFile)
+	if err != nil {
+		log.Fatalf("Failed to read WAV file: %v", err)
+	}
+
+	fmt.Println(samples)
 }
